636f5ba1dc988bbe294ce3cdee364ef235a0a8cd
LioExpleo <lionel.rocheteau@expleogroup.com>
2023-03-13 17:06:08 +0100

    Ok : Page d'inscription. Page de connexion Page d'abonnement. Page de flux :
        - Affichage dans l'ordre chrono mes critiques aux utilisateurs, les critiques et tickets des utilisateurs suivis.
        - Bouton Demander une critique ouvre la page de création de ticket
        - Créer une critique ouvre la page de création de critique et Review
        - La réponse au ticket ouvre la page de création de Review en réponse aux tickets.
         - La réponse aux critiques d'utilisateurs ayant fait leur propre critique à leur ticket affiche le bouton de création de critique. Si une autre critique est faite, le bouton n'est plus affiché.
    
    Page de création de critique fonctionne.
    Page de création de Critique et tickets fonctionne.
    Page de création de Critique en réponse aux tickets fonctionne.
    
    Page de posts :
        - Affichage de mes critiques dans l'ordre chrono fonctionne.
        - Review en réponse à mes propres tickets
        - Review en réponse à des tickets autres utilisateurs
        - Mes tickets
        - Bouton modifier review appelle la page de modification de Review
        - Bouton supprimer review appelle la page de suppression de Review
        - Bouton modifier ticket appelle la page de modification de ticket
        - Bouton supprimer ticket appelle la page de suppression de ticket
    
    Page de modification de Review fonctionne.
    Page de suppression de Review fonctionne.
    Page de modification de Ticket fonctionne.
    Page de suppression de Ticket fonctionne.
    
    Reste:
    Remise en forme générale avec balises et styles en cours.
    Nettoyage du code et de ce qui peut être supprimé.
    Contrôle du code flake 8

--- LitReview/app_crit/models.py
+++ LitReview/app_crit/models.py
@@ -23,6 +23,7 @@ class Ticket(models.Model):
         if self.image:
             self.resize_image()
 
+
 class Review(models.Model):
     ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
     rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
@@ -30,6 +31,7 @@ class Review(models.Model):
     headline = models.CharField(max_length=128, verbose_name="Titre")
     body = models.TextField(max_length=8192, blank=True, verbose_name="Commentaire")
     time_created = models.DateTimeField(auto_now_add=True)
+
 
 class UserFollows(models.Model):
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='following')
